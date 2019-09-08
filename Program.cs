ffbde4ce5a0f76555c6e47be98a736c88513494e
conoroshea1996 <conoshea12@gmail.com>
2019-09-08 13:07:26 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = System.Environment.MachineName;
+            Console.WriteLine(name);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
