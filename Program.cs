068dc70a3386041d420c5995a3a9a7ae18985f8e
conoroshea1996 <conoshea12@gmail.com>
2019-09-08 13:36:56 +0100

    variables

--- Program.cs
+++ Program.cs
@@ -23,6 +23,10 @@ namespace HelloWorld
             Console.WriteLine(firtname);
             Console.WriteLine(works);
 
+
+            Console.WriteLine("{0} {1} ", byte.MinValue,byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue , float.MaxValue);
         }
     }
 }
+
